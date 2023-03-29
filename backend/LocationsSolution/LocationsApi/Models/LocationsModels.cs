c1fa641cb66a9241b2d52d64774d18f80311ad10
Mary Fazio <mary_fazio@progressive.com>
2023-03-29 11:34:42 -0400

    Jeff's changes

--- backend/LocationsSolution/LocationsApi/Models/LocationsModels.cs
+++ backend/LocationsSolution/LocationsApi/Models/LocationsModels.cs
@@ -1,25 +1,23 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿namespace LocationsApi.Models;
 
-namespace LocationsApi.Models;
 
 public record LocationsResponse
 {
-    public IReadOnlyList<LocationItemResponse>? _embedded { get; set; }
+    public List<LocationsResponse> _embedded { get; set; } = new();
+    public record LocationItemResponse
+    {
+        public string Id { get; init; } = string.Empty;
+        public string Name { get; init; } = string.Empty;
+        public string Description { get; init; } = string.Empty;
+        public string AddedBy { get; init; } = string.Empty;
+        public DateTime AddedOn { get; init; }
+    }
 }
-public record LocationItemResponse
-{
-    public string Id { get; init; } = string.Empty;
-    public string Name { get; init; } = string.Empty;
-    public string Description { get; init; } = string.Empty;
-    public string AddedBy { get; init; } = string.Empty;
-    public DateTime AddedOn { get; init; }
-}
-
-public record LocationCreate
-{
-    [Required, MaxLength(75)]
-    public string Name { get; init; } = string.Empty;
-    [Required, MaxLength(1000)]
-    public string Description { get; init; } = string.Empty;
-
-}
+/*  {
+    id: '1',
+    name: "Aladdin's Eatery",
+    description:
+      'On Mayfield, good lunch time - lots of options for vegetarians',
+    addedBy: 'Bob',
+    addedOn: '2023-01-01',
+  } */
